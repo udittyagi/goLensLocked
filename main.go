aaed0afbe60638d1221fe93603b396fdd696d3e4
Udit Tyagi <udit.tyagi@olx.com>
2023-06-01 13:10:54 +0530

    Handler_Type

--- main.go
+++ main.go
@@ -11,29 +11,12 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello From Contact</h1>")
+	fmt.Fprint(w, "<h1>Hello From Contact Handler</h1>")
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-  // w.WriteHeader(http.StatusNotFound)
-  // fmt.Fprint(w, "Page Not Found")
-
-  //We can also use http.Error
-  // http.Error(w, "Page Not Found 2", http.StatusNotFound)
-
   http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 }
-
-// func pathHanlder(w http.ResponseWriter, r *http.Request) {
-// 	switch(r.URL.Path) {
-// 	case "/":
-// 		homeHandler(w, r)
-// 	case "/contact":
-// 		contactHandler(w, r)
-// 	default: 
-//     notFoundHandler(w, r)
-// 	}
-// }
 
 
 //We can use instance of Router in http.ListenAndServe as 2nd argument, since it implements the http.Handler interface 
@@ -52,16 +35,7 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
-  //HandleFunc are used only when DefaultServeMux is used, which in turn is used when nil is passed as second argument
-  // in  ==> http.ListenAndServe
-
-	// http.HandleFunc("/", pathHanlder)
-	// http.HandleFunc("/", homeHandler)
-	// http.HandleFunc("/contact", contactHandler)
-
   var router Router
 	fmt.Println("Starting Web Server on 3000...");
-	// http.ListenAndServe(":3000", nil)
 	http.ListenAndServe(":3000", router)
 }
