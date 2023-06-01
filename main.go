8dd71d57296342f0076028dde10ef75e95843cbf
Udit Tyagi <udit.tyagi@olx.com>
2023-06-01 17:26:24 +0530

    FAQ_Exercise

--- main.go
+++ main.go
@@ -18,12 +18,35 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
   http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "text/html; charset=utf-8")
+  fmt.Fprint(w, `<h1>FAQ Page</h1>
+<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+  </li>
+</ul>
+`)
+}
+
 func pathHandler(w http.ResponseWriter, r *http.Request) {
 	switch(r.URL.Path) {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+  case "/faq":
+		faqHandler(w, r)
 	default: 
     notFoundHandler(w, r)
 	}
