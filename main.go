3e2f32b7bcb4dd03af004304a8796d7fe483f91b
Udit Tyagi <udit.tyagi@olx.com>
2023-06-02 13:25:03 +0530

    Chi_Examples

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 
@@ -54,14 +55,24 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func MyRequestHandler(w http.ResponseWriter, r *http.Request) {
+  // fetch the url parameter `"userID"` from the request of a matching
+  // routing pattern. An example routing pattern could be: /users/{userID}
+  userID := chi.URLParam(r, "userID")
+  fmt.Fprint(w, fmt.Sprintf("<h1>User Key is %v</h1>", userID))
+}
+
 
 func main() {
   r := chi.NewRouter()
+  r.Use(middleware.Logger)
   r.Get("/", homeHandler)
   r.Get("/contact", contactHandler)
   r.Get("/faq", faqHandler)
+  r.Get("/users/{userID}", MyRequestHandler)
+
   r.NotFound(notFoundHandler)
 
 	fmt.Println("Starting Web Server on 3000...");
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3333", r)
 }
