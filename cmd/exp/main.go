28c1a4c9035847600a88a14f41c446dae6f5bc8b
Udit Tyagi <udit.tyagi@olx.com>
2023-06-02 17:00:03 +0530

    XSS

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -7,7 +7,8 @@ import (
 
 type Person struct {
 	Name string
-	Age int
+	Bio string
+	HtmlStr template.HTML
 }
 
 func main() {
@@ -16,8 +17,16 @@ func main() {
 		panic(err)
 	}
 
+	/**
+		If we use html/template then Bio will get excaped  &lt;script&gt;alert(&#34;Hello There!!&#34;)&lt;/script&gt;%
+		If we use text/templete then bio will not get escaped <script>alert("Hello There!!")</script>%
+
+		What if we want to set the HTML in our template.HTML type,  like we are  using in struct HtmlStr field
+	**/
 	p1 := Person {
-		"Udit", 28, 
+		Name: "Udit", 
+		Bio: `<script>alert("Hello There!!")</script>`,
+		HtmlStr: `<script>alert("Hello There!!")</script>`,
 	}
 	er := t.Execute(os.Stdout, p1)
 	if(err != nil) {
