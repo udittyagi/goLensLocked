71089f4ea55d7f38528aa21774612f04b7d6678e
Udit Tyagi <udit.tyagi@olx.com>
2023-06-02 10:27:02 +0530

    Starting Using Chi

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 
@@ -21,22 +23,22 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 func faqHandler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Content-Type", "text/html; charset=utf-8")
   fmt.Fprint(w, `<h1>FAQ Page</h1>
-<ul>
-  <li>
-    <b>Is there a free version?</b>
-    Yes! We offer a free trial for 30 days on any paid plans.
-  </li>
-  <li>
-    <b>What are your support hours?</b>
-    We have support staff answering emails 24/7, though response
-    times may be a bit slower on weekends.
-  </li>
-  <li>
-    <b>How do I contact support?</b>
-    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-  </li>
-</ul>
-`)
+  <ul>
+    <li>
+      <b>Is there a free version?</b>
+      Yes! We offer a free trial for 30 days on any paid plans.
+    </li>
+    <li>
+      <b>What are your support hours?</b>
+      We have support staff answering emails 24/7, though response
+      times may be a bit slower on weekends.
+    </li>
+    <li>
+      <b>How do I contact support?</b>
+      Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+    </li>
+  </ul>
+  `)
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
@@ -54,17 +56,12 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 
 
 func main() {
-  /*
-    We can use pathHandler in ListenAndServe becoz pathHandler has same type format as http.HandlerFunc
-    http.HandleFunc('/', pathHandler) method accepted the pathHandler as it accept function of  func(ResponseWriter, *Request) type.
-    But in order to send this to ListenAndServe we need to type convert the pathHandler to http.HandlerFunc which is basically type
-    with underlying type of  func(ResponseWriter, *Request), which our pathHandler matches and http.HandlerFunc also implements ServerHTTP method
-    so http.HandlerFunc implements http.Handler interface as well hence by type conveting our pathHandler, it will be a good candidate
-    to be passed in ListenAndServe
+  r := chi.NewRouter()
+  r.Get("/", homeHandler)
+  r.Get("/contact", contactHandler)
+  r.Get("/faq", faqHandler)
+  r.NotFound(notFoundHandler)
 
-    Inside http.HandleFunc('/', pathHandler), this conversion of pathHandler also occurs to use it with mux.Handle
-  */
-  var router http.HandlerFunc = pathHandler
 	fmt.Println("Starting Web Server on 3000...");
-	http.ListenAndServe(":3000", router)
+	http.ListenAndServe(":3000", r)
 }
