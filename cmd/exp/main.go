54c25619eb8b1a1072fba8bdbbe532355c8447ff
Udit Tyagi <udit.tyagi@olx.com>
2023-06-05 12:45:35 +0530

    Contextual Encoding

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -8,7 +8,7 @@ import (
 type Person struct {
 	Name string
 	Bio string
-	HtmlStr template.HTML
+	Age int
 }
 
 func main() {
@@ -18,15 +18,17 @@ func main() {
 	}
 
 	/**
-		If we use html/template then Bio will get excaped  &lt;script&gt;alert(&#34;Hello There!!&#34;)&lt;/script&gt;%
-		If we use text/templete then bio will not get escaped <script>alert("Hello There!!")</script>%
-
-		What if we want to set the HTML in our template.HTML type,  like we are  using in struct HtmlStr field
+		Go encode the data based on where it present, if it is in html it is encoded differently
+		if it is in script tag, it is encoded differently
+		In template Bio will be  &lt;script&gt;alert(&#34;Hello There!!&#34;)&lt;/script&gt;%
+		Inside script Bio will be "\u003cscript\u003ealert(\"Hello There!!\")\u003c/script\u003e"
+		Inside script the data type is also taken into consideration, Age field will be int in script
+		whereas string in template
 	**/
 	p1 := Person {
 		Name: "Udit", 
 		Bio: `<script>alert("Hello There!!")</script>`,
-		HtmlStr: `<script>alert("Hello There!!")</script>`,
+		Age: 24,
 	}
 	er := t.Execute(os.Stdout, p1)
 	if(err != nil) {
