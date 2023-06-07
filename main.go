9809c02249d53d88bc95a02c8453774501855af8
Udit Tyagi <udit.tyagi@olx.com>
2023-06-07 14:46:18 +0530

    FAQ Via Template

--- main.go
+++ main.go
@@ -38,29 +38,12 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
   executeTemplate(w, filePath);
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-  http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+  executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "text/html; charset=utf-8")
-  fmt.Fprint(w, `<h1>FAQ Page</h1>
-  <ul>
-    <li>
-      <b>Is there a free version?</b>
-      Yes! We offer a free trial for 30 days on any paid plans.
-    </li>
-    <li>
-      <b>What are your support hours?</b>
-      We have support staff answering emails 24/7, though response
-      times may be a bit slower on weekends.
-    </li>
-    <li>
-      <b>How do I contact support?</b>
-      Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
-    </li>
-  </ul>
-  `)
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+  http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 }
 
 func pathHandler(w http.ResponseWriter, r *http.Request) {
