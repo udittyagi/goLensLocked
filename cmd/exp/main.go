ebd45ce58a79f988f6700e4cbe096bf66a57ea79
Udit Tyagi <udit.tyagi@olx.com>
2023-07-06 12:16:27 +0530

    template Exercise

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -5,10 +5,19 @@ import (
 	"os"
 )
 
+type Address struct {
+	City string
+	Pin int64
+}
 type Person struct {
 	Name string
 	Bio string
 	Age int
+	AddressMap map[string]Address
+	Arr []int
+	ArrAddress []Address
+	IsLoading bool
+	IsActive bool
 }
 
 func main() {
@@ -16,20 +25,20 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-
-	/**
-		Go encode the data based on where it present, if it is in html it is encoded differently
-		if it is in script tag, it is encoded differently
-		In template Bio will be  &lt;script&gt;alert(&#34;Hello There!!&#34;)&lt;/script&gt;%
-		Inside script Bio will be "\u003cscript\u003ealert(\"Hello There!!\")\u003c/script\u003e"
-		Inside script the data type is also taken into consideration, Age field will be int in script
-		whereas string in template
-	**/
 	p1 := Person {
 		Name: "Udit", 
 		Bio: `<script>alert("Hello There!!")</script>`,
 		Age: 24,
+		AddressMap: map[string]Address{
+			"city1": {"Hapur", 245101},
+			"city2": {"Meertt", 245304},
+		},
+		Arr: []int{1,2,3},
+		ArrAddress: []Address{{"Hapur", 245101}, {"Meertt", 245304}},
+		IsLoading: false,
+		IsActive: true,
 	}
+
 	er := t.Execute(os.Stdout, p1)
 	if(err != nil) {
 		panic(er)
